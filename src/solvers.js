57741fec3a042005a4180932b8d8bf76ac6debde
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2017-02-16 11:19:04 -0800

    completed findnrookssolution method

--- src/solvers.js
+++ src/solvers.js
@@ -14,12 +14,36 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 
-
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  //TO DO: If there is a rowconflict, skip to next row
+  // return true if row conflict is found
+
+  var solution = new Board({n: n});
+  //0: row, 1: col
+  var row = arguments[1] ? arguments[1] : 0;
+  var col = arguments[2] ? arguments[2] : 0;
+
+  var placeNextRook = function(row, col) {
+    solution.togglePiece(row, col);
+
+    if (solution.hasRowConflictAt(row) || solution.hasColConflictAt(col)) {
+      solution.togglePiece(row, col);
+    }
+  };
+
+  solution.togglePiece(row, col);
+  //solution.togglePiece(0, 0);
+
+  for (var i = 0; i < n; i++) {
+    for (var j = 0; j < n; j++) {
+      if (i !== row && j !== col) {
+        placeNextRook(i, j);
+      }
+    }
+  }
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  return solution.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
